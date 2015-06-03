5b28eec354e072b8a5dd844c89fec33ae792b584
Jon <jonathanloui@gmail.com>
2015-06-03 04:31:15 -0700

    ninja_gold_game

---
+++ gold_game/phpadvanced_intermediate_proccess_jonathan_loui.php
@@ -0,0 +1,43 @@
+<?php
+	session_start();
+	
+	// outputs the responses
+	function entry($location, $min_gold, $max_gold)
+	{
+		if($location == "casino" && (rand(1,10) > 3))
+		{
+			$_SESSION['change'] = rand(-50, 0);
+			$log[] = "<p class='log2'>You entered the casino and lost " . $_SESSION['change'] . " golds...Ouch.. (" . 
+				date("F jS Y h:ia") . ")</p>";
+		}
+		else
+		{
+			$_SESSION['change'] = rand($min_gold, $max_gold);
+			$log[] = "<p class='log'>You entered the " . $location . " and earned " . $_SESSION['change'] . " golds. (" . 
+					date("F jS Y h:ia") . ")</p>";
+		}
+
+		$_SESSION['gold'] += $_SESSION['change'];
+		$_SESSION['log'][] = $log;
+		header("Location: phpadvanced_intermediate_jonathan_loui.php");
+		exit();
+	}
+
+	if(isset($_POST['location']))
+	{
+		if($_POST['location'] == "farm")
+			entry("farm", 10, 20);
+		else if($_POST['location'] == "cave")
+			entry("cave", 5, 10);
+		else if($_POST['location'] == "house")
+			entry("house", 2, 5);
+		else if($_POST['location'] == "casino")
+			entry("casino", 0, 50);
+	}
+	else
+	{
+		session_destroy();
+		header("Location: phpadvanced_intermediate_jonathan_loui.php");
+		die();
+	}
+?>
